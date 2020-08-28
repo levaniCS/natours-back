1d5051df7cd548e25aa98ce1308442c0b333ef4c
levaniCS <levanisarishvili3105@gmail.com>
2020-08-28 15:48:59 +0400

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,20 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //! 1) Global MIDDLEWARES\\
+
+//Access-Controll-Allow-Origin *
+app.use(cors());
+
+// api.natours.com
+// Front end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+// Allow non simple requests on cross (patch, put, delete)
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors())
+
 //* Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
